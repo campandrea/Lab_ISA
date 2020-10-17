99dd1af7ec4ef61e5bc74cfd2338b414b652e877
StefanoStfn <stfn.paletta@gmail.com>
2020-10-17 18:18:32 +0200

    Fixed number length problem

--- LAB1/testbench/Tb_filter_IIR.v
+++ LAB1/testbench/Tb_filter_IIR.v
@@ -10,6 +10,30 @@ wire VIN;
 wire [Nb-1:0] DOUT_Filter_TO_OUT_module;
 wire VOUT;
 wire EOF;
+
+///A coefficients
+wire signed [Nb-1:0] a0 =  10'd512;
+wire signed [Nb-1:0] a1 =  -10'd815;
+wire signed [Nb-1:0] a2 =  10'd1066;
+wire signed [Nb-1:0] a3 =  -10'd785;
+wire signed [Nb-1:0] a4 =  10'd445;
+wire signed [Nb-1:0] a5 =  -10'd164;
+wire signed [Nb-1:0] a6 =  10'd42;
+wire signed [Nb-1:0] a7 =  -10'd7;
+wire signed [Nb-1:0] a8 =  10'd0;
+
+///B coefficients
+wire signed [Nb-1:0] b0 =  10'd1;
+wire signed [Nb-1:0] b1 =  10'd9;
+wire signed [Nb-1:0] b2 =  10'd32;
+wire signed [Nb-1:0] b3 =  10'd65;
+wire signed [Nb-1:0] b4 =  10'd81;
+wire signed [Nb-1:0] b5 =  10'd65;
+wire signed [Nb-1:0] b6 =  10'd32;
+wire signed [Nb-1:0] b7 =  10'd9;
+wire signed [Nb-1:0] b8 =  10'd1;
+
+
 ///Net
 
 clk_gen 
@@ -57,13 +81,13 @@ IIR_filter
 		.DIN(DIN_IO_GEN_TO_Filter),
 		.VIN(~EOF),
 		///A coefficients
-		.a0(512), .a1(-815), .a2(1066), 
-		.a3(-785), .a4(445), .a5(-164), 
-		.a6(42), .a7(-7), .a8(0),
+		.a0(a0), .a1(a1), .a2(a2), 
+		.a3(a3), .a4(a4), .a5(a5), 
+		.a6(a6), .a7(a7), .a8(a8),
 		///B coefficients
-		.b0(1), .b1(9), .b2(32),
-		.b3(65), .b4(81), .b5(65), 
-		.b6(32), .b7(9), .b8(1),
+		.b0(b0), .b1(b1), .b2(b2),
+		.b3(b3), .b4(b4), .b5(b5), 
+		.b6(b6), .b7(b7), .b8(b8),
 		///Output
 		.DOUT(DOUT_Filter_TO_OUT_module),
 		.VOUT(VOUT)
