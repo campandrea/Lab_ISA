f27a79f7cc1b93923b2e54cdf9a9c570fda164ec
Gianluca Iacovelli <gianluca.iacovelli@studenti.polito.it>
2020-10-19 16:17:47 +0200

    matlab changes

--- LAB1/Matlab_scripts/myiir_design.m
+++ LAB1/Matlab_scripts/myiir_design.m
@@ -18,8 +18,8 @@ f0 = f_cut_off/f_nyq; %% Normalized cut-off frequency
 red=max(a);
 b=b./red
 a=a./red
-%a(1) = 1;
 [h1, w1]=freqz(b,a); %% %% get the transfer function of the designed filter
+
 
 bi=floor(b*2^(nb-1)); %% convert b coefficients into nb-bit integers
 bq=bi/2^(nb-1); %% convert back b coefficients as nb-bit real values
@@ -27,10 +27,15 @@ ai=floor(a*2^(nb-1)); %% convert a coefficients into nb-bit integers
 aq=ai/2^(nb-1); %% convert back a coefficients as nb-bit real values
 [h2, w2]=freqz(bq,aq); %% get the transfer function of the quantized filter
 
+a(1) = 1;
+[h3, w3] = freqz(b,a);
+
 %% show the transfer functions
 plot(w1/pi, 20*log10(abs(h1)));
 hold on;
 plot(w0/pi, 20*log10(abs(h0)), 'g');
+hold on;
+plot(w3/pi, 20*log10(abs(h3)), 'b');
 hold on;
 plot(w2/pi, 20*log10(abs(h2)),'r--');
 grid on;
