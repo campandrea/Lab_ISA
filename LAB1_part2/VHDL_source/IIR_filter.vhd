c8a5526c6288e1444b77ca59eec0a2ceabc894b1
andrea campanella <s234200@studenti.polito.it>
2020-11-02 16:19:35 +0100

    vhd files completi

--- LAB1_part2/VHDL_source/IIR_filter.vhd
+++ LAB1_part2/VHDL_source/IIR_filter.vhd
@@ -10,8 +10,8 @@ entity IIR_filter is
 			RST_n : IN std_logic;
 			DIN : IN signed (Nb-1 downto 0);
 			VIN : IN std_logic;
-			a0, a1 : IN signed (Nb-1 downto 0);
-			b0, b1 : IN signed (Nb-1 downto 0);
+			a0, a1, a2 : IN signed (Nb-1 downto 0);
+			b0, b1, b2 : IN signed (Nb-1 downto 0);
 
 			DOUT : OUT signed(Nb-1 downto 0);
 			VOUT : OUT std_logic
@@ -76,14 +76,15 @@ architecture structure of IIR_filter is
     end component;
 
 	--signals
-	signal a0_in, a1_in, b0_in, b1_in : signed(N_par -1 downto 0);
+	signal a0_in, a1_in, a2_in, b0_in, b1_in, b2_in : signed(N_par -1 downto 0);
 	
-	signal prod_a1 : signed(N_par -1 downto 0); --multiplication results
-	signal prod_b0, prod_b1 : signed(N_par -1 downto 0);
-	signal prod_b0_2N, prod_a1_2N, prod_b1_2N : signed(2*N_par-1 downto 0);
+	signal sum_a_in_a, sum_a_in_b, sum_a_out : signed(N_par -1 downto 0);
+	signal sum_b_in_a, sum_b_in_b, sum_b_out : signed(N_par -1 downto 0);
+	signal sum_b1b2_b1, sum_b1b2_b2, sum_b1b2_out: signed(N_par -1 downto 0);
 	
-	signal reg_out : signed(N_par -1 downto 0);
-	signal sum_a0, sum_b0 : signed(N_par -1 downto 0); --sum results
+	signal mult_a2_out_2N, mult_b0_out_2N, mult_b1_out_2N, mult_b2_out_2N : signed(2*N_par-1 downto 0);
+	signal mult_a2_out, mult_b0_out, mult_b1_out, mult_b2_out : signed(N_par-1 downto 0);
+	signal mult_a2_in, mult_b0_in, mult_b1_in, mult_b2_in : signed(N_par -1 downto 0);
 
 	signal rst : std_logic;
 	signal count_tc : std_logic;
@@ -96,9 +97,11 @@ begin
 
 	a0_in <= resize(a0, N_par);
 	a1_in <= resize(a1, N_par);
+	a2_in <= resize(a2, N_par);
 
 	b0_in <= resize(b0, N_par);
 	b1_in <= resize(b1, N_par);
+	b2_in <= resize(b2, N_par);
 
 	rst <= not RST_n;
 
@@ -122,61 +125,165 @@ begin
 			D => VIN,
 			Q => VIN_samp
 		);
+		
+		
+	sum_a_in_a <= DIN_samp;
+	
+	sum_a : adder
+	generic map (N => N_par)
+	port map (
+		A => sum_a_in_a,
+		B => sum_a_in_b,
+		R => sum_a_out
+	);
+	
+	sum_b : adder
+	generic map (N => N_par)
+	port map (
+		A => sum_b_in_a,
+		B => sum_b_in_b,
+		R => sum_b_out
+	);
+	
+	sum_b1b2 : adder
+	generic map (N => N_par)
+	port map (
+		A => sum_b1b2_b1,
+		B => sum_b1b2_b2,
+		R => sum_b1b2_out
+	);
 
-	reg0 : RegisterSigned
+	mult_a2 : multiplier
+		generic map (N => N_par)
+		port map (
+			A => mult_a2_in,
+			B => a2_in,
+			R => mult_a2_out_2N
+		);
+	
+	mult_b0 : multiplier
+		generic map (N => N_par)
+		port map (
+			A => mult_b0_in,
+			B => b0_in,
+			R => mult_b0_out_2N
+		);
+	
+	mult_b1 : multiplier
+		generic map (N => N_par)
+		port map (
+			A => mult_b1_in,
+			B => b1_in,
+			R => mult_b1_out_2N
+		);
+		
+	mult_b2 : multiplier
+		generic map (N => N_par)
+		port map (
+			A => mult_b2_in,
+			B => b2_in,
+			R => mult_b2_out_2N
+		);
+	
+	-- tolti gli MSB derivanti dalla moltiplicazione e alcuni LSB
+	mult_a2_out <= mult_a2_out_2N(N_par +Nb -2 downto Nb -1);
+	mult_b0_out <= mult_b0_out_2N(N_par +Nb -2 downto Nb -1);
+	mult_b1_out <= mult_b1_out_2N(N_par +Nb -2 downto Nb -1);
+	mult_b2_out <= mult_b2_out_2N(N_par +Nb -2 downto Nb -1);
+	 
+	
+	reg_a2_out : RegisterSigned
 		generic map (N => N_par)
 		port map (
 			clk => CLK,
 			en => VIN_samp,
 			rst => rst,
-			D => sum_a0,
-			Q => reg_out
+			D => mult_a2_out,
+			Q => sum_a_in_b
 		);
-
-	mult_b0 : multiplier
+		
+	reg_mid_1 : RegisterSigned
 		generic map (N => N_par)
 		port map (
-			A => sum_a0,
-			B => b0_in,
-			R => prod_b0_2N
+			clk => CLK,
+			en => VIN_samp,
+			rst => rst,
+			D => sum_a_out,
+			Q => mult_b1_in
 		);
+		
+	reg_mid_2 : RegisterSigned
+		generic map (N => N_par)
+		port map (
+			clk => CLK,
+			en => VIN_samp,
+			rst => rst,
+			D => mult_b1_in,
+			Q => mult_a2_in
+		);
+		
+	reg_b0_in : RegisterSigned
+		generic map (N => N_par)
+		port map (
+			clk => CLK,
+			en => VIN_samp,
+			rst => rst,
+			D => sum_a_out,
+			Q => mult_b0_in
+	);
+		
+	reg_b0_out : RegisterSigned
+		generic map (N => N_par)
+		port map (
+			clk => CLK,
+			en => VIN_samp,
+			rst => rst,
+			D => mult_b0_out,
+			Q => sum_b_in_a
+		);
+		
+	reg_b1_out : RegisterSigned
+		generic map (N => N_par)
+		port map (
+			clk => CLK,
+			en => VIN_samp,
+			rst => rst,
+			D => mult_b1_out,
+			Q => sum_b1b2_b1
+		);
+		
+	reg_b2_in : RegisterSigned
+		generic map (N => N_par)
+		port map (
+			clk => CLK,
+			en => VIN_samp,
+			rst => rst,
+			D => mult_a2_in,
+			Q => mult_b2_in
+		);
+		
+	reg_b2_out : RegisterSigned
+		generic map (N => N_par)
+		port map (
+			clk => CLK,
+			en => VIN_samp,
+			rst => rst,
+			D => mult_b2_out,
+			Q => sum_b1b2_b2
+		);
+		
+	reg_b1b2_out : RegisterSigned
+		generic map (N => N_par)
+		port map (
+			clk => CLK,
+			en => VIN_samp,
+			rst => rst,
+			D => sum_b1b2_out,
+			Q => sum_b_in_b
+		);
+		
 	-- tolti gli MSB derivanti dalla moltiplicazione e gli LSB
-	prod_b0 <= prod_b0_2N(N_par +Nb -2 downto Nb -1); 
 	
-	mult_b1 : multiplier
-		generic map (N => N_par)
-		port map (
-			A => reg_out,
-			B => b1_in,
-			R => prod_b1_2N
-		);
-	prod_b1 <= prod_b1_2N(N_par +Nb -2 downto Nb -1);
-	
-	add_b0 : adder
-	generic map (N => N_par)
-	port map (
-		A => prod_b0,
-		B => prod_b1,
-		R => sum_b0
-	);
-
-
-	mult_a0 : multiplier
-	generic map ( N => N_par )
-	port map (
-		A => reg_out,
-		B => a1_in,
-		R => prod_a1_2N
-	);
-	prod_a1 <= prod_a1_2N(N_par +Nb -2 downto Nb -1);
-
-	add_a0 : adder
-		generic map (N => N_par)
-		port map (
-			A => DIN_samp,
-			B => prod_a1,
-			R => sum_a0
-		);
 
 	count : Counter
 		generic map (N => 2)
@@ -184,9 +291,18 @@ begin
 			clk => CLK,
 			CE => VIN_samp,
 			RST => rst,
-			COUNT_MAX => "01",
+			COUNT_MAX => "11",
 			TC => count_tc
 		);
+		
+	saturation: Saturator
+		generic map ( 
+			N_in => N_par,
+			N_out => Nb)
+		port map(
+			NUM_IN => sum_b_out,
+			NUM_OUT => sat_out
+	);
 
 	DOUT_reg : RegisterSigned
 		generic map ( N => Nb )
@@ -207,12 +323,6 @@ begin
 			Q => VOUT
 		);
 
-    saturation: Saturator
-        generic map ( N_in => N_par,
-                      N_out => Nb)
-        port map(
-            NUM_IN => sum_b0,
-            NUM_OUT => sat_out
-        );
+
 
 end architecture structure;
