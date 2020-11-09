38ee199db52412cee856ba0f436c06c14f09f73b
StefanoStfn <stfn.paletta@gmail.com>
2020-11-09 20:59:09 +0100

    modified C files and matlab files too

--- LAB1/Python_scripts/error_check.py
+++
@@ -1,8 +0,0 @@
-import functions as f
-from sys import argv
-
-if len(argv) != 4:
-	print("ERROR\nCorrect usage: input_gen.py <DUT_output> <C_output> <error_file>")
-	exit()
-dutfile = f.eliminateLineFile(argv[1],2)
-f.generateErrorFile(dutfile, argv[2], argv[3])
