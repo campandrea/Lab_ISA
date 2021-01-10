c3d361061f15e306932a694d5c6890beed6a0d4a
Gianluca Iacovelli <>
2021-01-10 11:54:21 +0100

    aggiustato memoria e inizio testbench

--- LAB3/testbench/Memory.vhd
+++ LAB3/testbench/Memory.vhd
@@ -3,40 +3,52 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Memory is
-	generic (word_size 	: integer := 8;
-			 addr_size 	: integer := 10
+	generic (word_size : integer := 8;
+			     addr_size : integer := 32;
+           line_size : integer := 4 -- number of words to take each time
             );
 	port (clk	   : in std_logic;
-		  chip_sel : in std_logic;
-		  rd	   : in std_logic;
-          wr_n     : in std_logic;
-		  addr	   : in std_logic_vector(addr_size - 1 downto 0);
-		  data_in  : in std_logic_vector(word_size - 1 downto 0);
-		  data_out : in std_logic_vector(word_size - 1 downto 0)
+  		  chip_sel : in std_logic;
+  		  rd	   : in std_logic;
+        wr_n     : in std_logic;
+  		  addr	   : in std_logic_vector(addr_size - 1 downto 0);
+  		  data_in  : in std_logic_vector(line_size - 1 downto 0);
+  		  data_out : in std_logic_vector(line_size - 1 downto 0)
          );
 end Memory;
 
 architecture Behavior of Memory is
-	subtype data is std_logic_vector(word_size - 1 downto 0);
-	type memory_type is array(0 to (2 ** addr_size) - 1) of data;
+  subtype data is std_logic_vector(word_size - 1 downto 0);
+  type memory_type is array(0 to (2 ** addr_size) - 1) of data;
 
 	signal memory : memory_type;
 begin
+
 	write_proc: process(clk)
+    variable int_addr : integer;
 	begin
+    int_addr := to_integer(unsigned(addr));
 		if rising_edge(clk) then
 			if chip_sel = '1' then
 				if wr_n = '0' then
-					memory(to_integer(addr)) <= data_in;
+          for i in 0 to line_size loop
+					     memory(int_addr + i) <= data_in(31 - i*wordsize downto 24 - i*wordsize);
+          end loop;
 				end if;
 			end if;
 		end if;
 	end process;
 
-    read_proc: process(addr)
+    read_proc: process(addr, rd)
+      variable int_addr : integer;
+      variable data_out_var : std_logic_vector(31 downto 0);
     begin
+        int_addr := to_integer(unsigned(addr));
         if rd = '1' then
-            data_out <= memory(to_integer(addr));
+          for i in 0 to line_size loop 
+            data_out_var(31 - i*wordsize downto 24 - i*wordsize) := memory(int_addr + i);
+          end loop;
         end if;
+        data_out <= data_out_var;
     end process;
 END architecture Behavior;
--- LAB3/testbench/tb_riscV.v
+++ LAB3/testbench/tb_riscV.v
@@ -3,18 +3,45 @@ module tb_riscV
  (output wire END_SIM_i);
 
 wire CLK;
+wire RST_n
 reg END_SIM_reg;
+
 
 initial begin
 	#1 END_SIM_reg <= 0;
+  //parte il clock e il suo enable
+  // parte la lettura del file e la scrittura nell'instruction memory
+  // quando finisce dare il reset alla dut e farla partire
+  //aspettare un po di tempo
+  // mettere end_sim_i a 1
 end
+
+//task per controllare a chi dare il comando della data memory
+task write_data_memory_handler();
+  input END_sim_i;
+  output //aggiungere i segnali utili
+  begin
+    if(END_sim_i == 0)
+      begin
+      // il comando della data mem deve essere della DUT
+      end
+    else if(END_sim_i == 1)
+      begin
+      //il comando deve averlo il dut
+      end
+endtask
+
+//effettuare sempre il controllo
+always write_data_memory_handler();
+
+
 //Clock generator
 //AGGIUSTARE
 clk_gen
 	CLK_Module(
 	.EN_N(END_SIM_reg),
 	.CLK(CLK),
-	.RST_n()
+	.RST_n(RST_n)
 );
 
  ///Read instruction from file
@@ -29,13 +56,51 @@ Stimuli_generator
 	);
 
 //Instruction memory
-//AGGIUNGERE
+//AGGIUSTARE
+Memory
+#(.word_size(8),
+  .addr_size(32),
+  .line_size(4)
+  )
+  Instruction_mem_module(
+      .chip_sel(),
+      .rd(),
+      .wr_n(),
+      .addr(),
+      .data_in(),
+      .data_out()
+  );
 
 //DUT riscV processor
-//AGGIUNGERE
+//AGGIUSTARE
+datapath
+    DUT_module(
+        .clk(CLK),
+        .rst(),
+        .PCOut(),
+        .Instruction(),
+        .DataMemRead(),
+        .DataMemWrite(),
+        .DataMemAddr(),
+        .DataMemDataIn(),
+        .DataMemDataOut()
+    );
 
 //Data memory
-//AGGIUNGERE
+//AGGIUSTARE
+Memory
+#(.word_size(8),
+  .addr_size(32),
+  .line_size(4)
+  )
+  Data_mem_module(
+      .chip_sel(),
+      .rd(),
+      .wr_n(),
+      .addr(),
+      .data_in(),
+      .data_out()
+  );
 
 //Write result data on file
 //AGGIUSTARE
@@ -47,8 +112,14 @@ Output_Sink
 		.data_in()
 	);
 
+
+
+
 always @ (END_SIM_i) begin
 	if (END_SIM_i == 1) begin
+    //trascrivere il contenuto della data memory sul file
+    // per il segnale di rd che dovrebbe essere dato solo da DUT
+    // potrebbero esserci dei conflitti
 		$toggle_stop;
 	end
 end
