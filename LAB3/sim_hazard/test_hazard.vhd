28039b786d4aa99f37730e5c0f5c957f5c292f58
Gianluca Iacovelli <>
2021-01-20 00:01:49 +0100

    aggiunto tb reg file e hazard

---
+++ LAB3/sim_hazard/test_hazard.vhd
@@ -0,0 +1,83 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity test_hazard is
+
+end entity;
+
+architecture behaviour of test_hazard is
+
+component HazardDetUnit
+port(
+  Rs1_IF : in std_logic_vector (4 downto 0);
+  Rs2_IF : in std_logic_vector (4 downto 0);
+  Rd_ID : in std_logic_vector (4 downto 0);
+  MemRead_ID : in std_logic;
+  BrEq : in std_logic;
+  BrInstr_ID : in std_logic;
+  IF_RegEn : out std_logic;
+  PC_RegEn : out std_logic;
+  ID_RegSel : out std_logic;
+  IF_RegSel : out std_logic;
+  PCSel : out std_logic
+);
+end component;
+
+signal  Rs1_IF, Rs2_IF, Rd_ID : std_logic_vector (4 downto 0);
+signal  MemRead_ID, BrEq, BrInstr_ID : std_logic;
+signal  IF_RegEn, PC_RegEn, ID_RegSel, IF_RegSel, PCSel : std_logic;
+
+
+begin
+DUT : HazardDetUnit
+port map(
+  Rs1_IF => Rs1_IF ,
+  Rs2_IF => Rs2_IF
+  Rd_ID => Rd_ID,
+  MemRead_ID => MemRead_ID,
+  BrEq => BrEq,
+  BrInstr_ID => BrInstr_ID,
+  IF_RegEn => IF_RegEn,
+  PC_RegEn => PC_RegEn,
+  ID_RegSel => ID_RegSel,
+  IF_RegSel => IF_RegSel,
+  PCSel => PCSel
+);
+
+process
+begin
+	Rs1_IF <= (others => 'U');
+	Rs2_IF <= (others => 'U');
+	Rd_ID <= (others => 'U');
+  MemRead_ID <= 'U';
+  BrEq <= 'U';
+  BrInstr_ID <= 'U';
+	wait for 2 ns;
+  Rs1_IF <= (others => 'X');
+  Rs2_IF <= (others => 'X');
+  Rd_ID <= (others => 'X');
+  MemRead_ID <= 'X';
+  BrEq <= 'X';
+  BrInstr_ID <= 'X';
+	wait for 2 ns;
+  Rs1_IF <= '101011';
+  Rs1_ID <= '101011';
+  MemRead_ID <= '1';
+	wait for 2 ns;
+  Rs1_IF <= (others => 'X');
+  Rd_ID <= (others => 'X');
+  MemRead_ID <= 'X';
+	wait for 2 ns;
+  Rs2_IF <= '101110';
+  Rs1_ID <= '101110';
+  MemRead_ID <= '1';
+	wait for 2 ns;
+  BrInstr_ID <= '1';
+  wait for 2 ns;
+  BrEq <= '1';
+	wait;
+end process;
+
+end behaviour;
---
+++ LAB3/sim_register/test_register.vhd
@@ -0,0 +1,124 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity test_register is
+
+end entity;
+
+architecture behaviour of test_register is
+
+  component Register_PC is
+  generic( N : natural := 32);
+  port(
+        data_in  : in std_logic_vector (N-1 downto 0);
+        clk      : in std_logic;
+        reg_rst  : in std_logic;
+        reg_en   : in std_logic;
+        data_out : out std_logic_vector (N-1 downto 0)
+  );
+end component;
+
+component Register_std is
+port(
+      data_in  : in std_logic;
+      clk      : in std_logic;
+      reg_rst  : in std_logic;
+      reg_en   : in std_logic;
+      data_out : out std_logic
+);
+end component;
+
+component Register_vec is
+generic( N : natural := 32);
+port(
+      data_in  : in std_logic_vector (N-1 downto 0);
+      clk      : in std_logic;
+      reg_rst  : in std_logic;
+      reg_en   : in std_logic;
+      data_out : out std_logic_vector (N-1 downto 0)
+);
+end component;
+
+signal data_in_vec, data_in_pc : std_logic_vector (31 downto 0);
+signal data_in_std : std_logic;
+signal clk      : std_logic;
+signal reg_rst  : std_logic;
+signal reg_en   : std_logic;
+signal data_out_vec, data_out_pc : std_logic_vector (31 downto 0)
+signal data_out_std : std_logic;
+
+
+
+begin
+
+DUT1 : Register_PC
+port map(
+    data_in => data_in_pc,
+    clk => clk,
+    reg_rst => reg_rst,
+    reg_en => reg_en,
+    data_out => data_out_pc
+);
+
+DUT2 : Register_std
+port map(
+    data_in => data_in_std,
+    clk => clk,
+    reg_rst => reg_rst,
+    reg_en => reg_en,
+    data_out => data_out_std
+);
+
+DUT3 : Register_vec
+port map(
+    data_in => data_in_vec,
+    clk => clk,
+    reg_rst => reg_rst,
+    reg_en => reg_en,
+    data_out => data_out_vec
+
+    clk_proc : process
+  	begin
+  		clk <= 'U';
+  		wait for 2 ns;
+  		clk <= 'X';
+  		wait for 3 ns;
+  		for i in 0 to 1024 loop
+  			clk <= '0';
+  			wait for 2 ns;
+  			clk <= '1';
+  			wait for 2 ns;
+  		end loop;
+  	end process;
+
+
+process
+begin
+	data_in_vec <= (others => 'U');
+	data_in_pc <= (others => 'U');
+	data_in_std <= 'U';
+  reg_rst <= 'U';
+  reg_en <= 'U';
+	wait for 2 ns;
+  data_in_vec <= (others => 'X');
+	data_in_pc <= (others => 'X');
+	data_in_std <= 'X';
+  reg_rst <= 'X';
+  reg_en <= 'X';
+	wait for 2 ns;
+  reg_rst <= '1';
+	wait for 2 ns;
+  reg_rst <= '0';
+  reg_en <= '1';
+	wait for 2 ns;
+  data_in_vec <= "1010110011110000101010101111010";
+	data_in_pc <=  "1111111110000001111111100011110";
+	data_in_std <= '1';
+	wait for 2 ns;
+  reg_en <= '0';
+	wait;
+end process;
+
+end behaviour;
